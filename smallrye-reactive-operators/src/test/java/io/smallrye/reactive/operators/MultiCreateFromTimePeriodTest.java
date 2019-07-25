eaacfa419429c3fcba1fb86005457f301ddb70f5
Clement Escoffier <clement.escoffier@gmail.com>
2019-07-25 12:09:00 +0200

    Relax time constraints

--- smallrye-reactive-operators/src/test/java/io/smallrye/reactive/operators/MultiCreateFromTimePeriodTest.java
+++ smallrye-reactive-operators/src/test/java/io/smallrye/reactive/operators/MultiCreateFromTimePeriodTest.java
@@ -45,7 +45,7 @@ public class MultiCreateFromTimePeriodTest {
         List<Long> list = ts.results();
         for (int i = 0; i < list.size() - 1; i++) {
             long delta = list.get(i + 1) - list.get(i);
-            assertThat(delta).isBetween(50L, 350L);
+            assertThat(delta).isBetween(20L, 350L);
         }
     }
 
@@ -71,7 +71,7 @@ public class MultiCreateFromTimePeriodTest {
         List<Long> list = ts.results();
         for (int i = 0; i < list.size() - 1; i++) {
             long delta = list.get(i + 1) - list.get(i);
-            assertThat(delta).isBetween(50L, 350L);
+            assertThat(delta).isBetween(20L, 350L);
         }
     }
 
