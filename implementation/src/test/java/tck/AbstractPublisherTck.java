3f4bdf50df29926dbc7f38182da7dbc5058d55f4
Clement Escoffier <clement.escoffier@gmail.com>
2019-12-03 10:58:24 +0100

    Increase timeout in TCK.

--- implementation/src/test/java/tck/AbstractPublisherTck.java
+++ implementation/src/test/java/tck/AbstractPublisherTck.java
@@ -15,7 +15,7 @@ import io.smallrye.mutiny.Multi;
 public abstract class AbstractPublisherTck<T> extends PublisherVerification<T> {
 
     public AbstractPublisherTck() {
-        this(10L);
+        this(500L);
     }
 
     public AbstractPublisherTck(long timeout) {
