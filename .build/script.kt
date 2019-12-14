52336acfd4e71b82fbdf46c80da286f893bce429
Clement Escoffier <clement.escoffier@gmail.com>
2019-12-14 18:48:34 +0100

    change script extension

--- .build/script.kt
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env kscript
-println("Hello! " + args[1] + " " + args[2]);
---
+++ .build/script.kts
@@ -0,0 +1,6 @@
+#!/usr/bin/env kscript
+
+val event_type = args[1]
+val client_payload = args[2]
+
+println("Hello! ${event_type} ${client_payload}");
