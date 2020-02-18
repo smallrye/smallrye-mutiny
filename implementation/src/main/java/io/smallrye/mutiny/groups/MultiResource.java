274ddde0d42022bc52687360b5fe3a420a100c29
Clement Escoffier <clement.escoffier@gmail.com>
2020-02-18 10:53:50 +0100

    Provides the Multi.createFrom().resource() builder
    This builder allows attaching finalizers called when the stream completes, fails or when the subscriber cancels.
    It is useful to perform cleanup after consumption or failure.
    
    Re-order imports

---
+++ implementation/src/main/java/io/smallrye/mutiny/groups/MultiResource.java
@@ -0,0 +1,103 @@
+package io.smallrye.mutiny.groups;
+
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.reactivestreams.Publisher;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.helpers.ParameterValidation;
+import io.smallrye.mutiny.operators.multi.builders.ResourceMulti;
+
+/**
+ * Allows configuring a <em>finalizer</em> to close the resource attached to the stream.
+ *
+ * @param <R> the type of resource
+ * @param <I> the type of item emitted by the resulting {@code Multi}
+ * @see MultiCreate#resource(Supplier, Function)
+ */
+public class MultiResource<R, I> {
+    private final Function<? super R, ? extends Publisher<I>> streamSupplier;
+    private final Supplier<? extends R> resourceSupplier;
+
+    public MultiResource(Supplier<? extends R> resourceSupplier,
+            Function<? super R, ? extends Publisher<I>> streamSupplier) {
+        this.resourceSupplier = resourceSupplier;
+        this.streamSupplier = streamSupplier;
+    }
+
+    /**
+     * Configures a <em>synchronous</em> finalizer. The given function is called when the stream completes, fails or
+     * when the subscriber cancels.
+     * If the finalizer throws an exception, this exception is propagated to the subscriber, unless it has already
+     * cancelled.
+     *
+     * @param finalizer the finalizer, must not be {@code null}
+     * @return the multi
+     */
+    public Multi<I> withFinalizer(Consumer<? super R> finalizer) {
+        ParameterValidation.nonNull(finalizer, "finalizer");
+        Function<? super R, Uni<Void>> actual = r -> {
+            finalizer.accept(r);
+            return Uni.createFrom().item((Void) null);
+        };
+        return withFinalizer(actual, (r, ignored) -> actual.apply(r), actual);
+    }
+
+    /**
+     * Configures an <em>asynchronous</em> finalizer. The given function is called when the stream completes, fails or
+     * when the subscriber cancels. The returned {@code Uni} is flattened with the stream meaning that the subscriber
+     * gets the events fired by the {@code Uni}. If the {@link Uni} completes successfully, the subscriber gets
+     * the {@code completion} event. If the {@link Uni} fails, the subscriber gets the failure even if the resource
+     * stream completed successfully. If the {@link Uni} fails after a resource stream failure, the subscriber receives
+     * a {@link io.smallrye.mutiny.CompositeException}. If the subscribers cancels, the {@link Uni} outcome is ignored.
+     * <p>
+     * If the finalizer throws an exception, this exception is propagated to the subscriber, unless it has already
+     * cancelled.
+     * If the finalizer returns {@code null}, a {@link NullPointerException} is propagated to the subscriber, unless it
+     * has already cancelled.
+     *
+     * @param finalizer the finalizer, must not be {@code null}
+     * @return the multi
+     */
+    public Multi<I> withFinalizer(Function<? super R, Uni<Void>> finalizer) {
+        Function<? super R, Uni<Void>> actual = ParameterValidation.nonNull(finalizer, "finalizer");
+        return withFinalizer(actual, (r, ignored) -> actual.apply(r), actual);
+    }
+
+    /**
+     * Configures <em>asynchronous</em> finalizers distinct for each event. The given functions are called when the
+     * stream completes, fails or when the subscriber cancels.
+     * <p>
+     * The returned {@code Uni} is flattened with the stream meaning that the subscriber
+     * gets the events fired by the {@code Uni}. If the {@link Uni} completes successfully, the subscriber gets
+     * the {@code completion} event. If the {@link Uni} fails, the subscriber gets the failure even if the resource
+     * stream completed successfully. If the {@link Uni} fails after a resource stream failure, the subscriber receives
+     * a {@link io.smallrye.mutiny.CompositeException}. If the subscribers cancels, the {@link Uni} outcome is ignored.
+     * <p>
+     * If a finalizer throws an exception, this exception is propagated to the subscriber, unless it has already
+     * cancelled.
+     * If a finalizer returns {@code null}, a {@link NullPointerException} is propagated to the subscriber, unless it
+     * has already cancelled.
+     *
+     * @param onCompletion the completion finalizer called when the resource stream completes successfully. Must not be
+     *        {@code null}
+     * @param onFailure the failure finalizer called when the resource stream propagated a failure. The finalizer is
+     *        called with the resource and the failure. Must not be {@code null}
+     * @param onCancellation the cancellation finalizer called when the subscribers cancels the subscription. Must not
+     *        be {@code null}.
+     * @return the multi
+     */
+    public Multi<I> withFinalizer(
+            Function<? super R, Uni<Void>> onCompletion,
+            BiFunction<? super R, ? super Throwable, Uni<Void>> onFailure,
+            Function<? super R, Uni<Void>> onCancellation) {
+        return new ResourceMulti<>(resourceSupplier, streamSupplier,
+                ParameterValidation.nonNull(onCompletion, "onCompletion"),
+                ParameterValidation.nonNull(onFailure, "onFailure"),
+                ParameterValidation.nonNull(onCancellation, "onCancellation"));
+    }
+}
---
+++ implementation/src/main/java/io/smallrye/mutiny/operators/multi/builders/ResourceMulti.java
@@ -0,0 +1,201 @@
+package io.smallrye.mutiny.operators.multi.builders;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscription;
+
+import io.smallrye.mutiny.CompositeException;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.helpers.ParameterValidation;
+import io.smallrye.mutiny.helpers.Subscriptions;
+import io.smallrye.mutiny.operators.AbstractMulti;
+import io.smallrye.mutiny.subscription.MultiSubscriber;
+
+public class ResourceMulti<R, I> extends AbstractMulti<I> {
+    private final Supplier<? extends R> resourceSupplier;
+    private final Function<? super R, ? extends Publisher<I>> streamSupplier;
+    private final Function<? super R, Uni<Void>> onCompletion;
+    private final BiFunction<? super R, ? super Throwable, Uni<Void>> onFailure;
+    private final Function<? super R, Uni<Void>> onCancellation;
+
+    public ResourceMulti(
+            Supplier<? extends R> resourceSupplier,
+            Function<? super R, ? extends Publisher<I>> streamSupplier,
+            Function<? super R, Uni<Void>> onCompletion,
+            BiFunction<? super R, ? super Throwable, Uni<Void>> onFailure,
+            Function<? super R, Uni<Void>> onCancellation) {
+        this.resourceSupplier = resourceSupplier;
+        this.streamSupplier = streamSupplier;
+        this.onCompletion = onCompletion;
+        this.onFailure = onFailure;
+        this.onCancellation = onCancellation;
+    }
+
+    @Override
+    public void subscribe(MultiSubscriber<? super I> subscriber) {
+        R resource;
+        try {
+            resource = resourceSupplier.get();
+            if (resource == null) {
+                throw new IllegalArgumentException(ParameterValidation.SUPPLIER_PRODUCED_NULL);
+            }
+        } catch (Throwable e) {
+            Subscriptions.fail(subscriber, e);
+            return;
+        }
+
+        Publisher<? extends I> stream;
+        try {
+            stream = streamSupplier.apply(resource);
+            if (stream == null) {
+                throw new IllegalArgumentException(ParameterValidation.SUPPLIER_PRODUCED_NULL);
+            }
+        } catch (Throwable e) {
+            try {
+                Uni<Void> uni = onFailure.apply(resource, e);
+                if (uni == null) {
+                    Subscriptions.fail(subscriber,
+                            new NullPointerException("Unable to call the finalizer - it returned `null`"));
+                } else {
+                    uni.subscribe().with(
+                            completed -> Subscriptions.fail(subscriber, e),
+                            failed -> Subscriptions.fail(subscriber, new CompositeException(e, failed)));
+                }
+            } catch (Throwable ex) {
+                Subscriptions.fail(subscriber, new CompositeException(e, ex));
+                return;
+            }
+            Subscriptions.fail(subscriber, e);
+            return;
+        }
+
+        ResourceSubscriber<I, R> us = new ResourceSubscriber<>(subscriber, resource, onCompletion, onFailure,
+                onCancellation);
+        stream.subscribe(us);
+    }
+
+    private static class ResourceSubscriber<I, R> implements Subscription, MultiSubscriber<I> {
+
+        private final MultiSubscriber<? super I> downstream;
+        private final R resource;
+        private final Function<? super R, Uni<Void>> onCompletion;
+        private final BiFunction<? super R, ? super Throwable, Uni<Void>> onFailure;
+        private final Function<? super R, Uni<Void>> onCancellation;
+        private final AtomicBoolean terminated = new AtomicBoolean();
+        private final AtomicReference<Subscription> upstream = new AtomicReference<>();
+
+        public ResourceSubscriber(
+                MultiSubscriber<? super I> downstream,
+                R resource,
+                Function<? super R, Uni<Void>> onCompletion,
+                BiFunction<? super R, ? super Throwable, Uni<Void>> onFailure,
+                Function<? super R, Uni<Void>> onCancellation) {
+            this.downstream = downstream;
+            this.resource = resource;
+            this.onCompletion = onCompletion;
+            this.onFailure = onFailure;
+            this.onCancellation = onCancellation;
+        }
+
+        @Override
+        public void onSubscribe(Subscription s) {
+            if (upstream.compareAndSet(null, s)) {
+                downstream.onSubscribe(this);
+            }
+        }
+
+        @Override
+        public void onItem(I item) {
+            downstream.onNext(item);
+        }
+
+        @Override
+        public void onFailure(Throwable failure) {
+            Throwable innerError = null;
+            Uni<Void> uni = null;
+            if (terminated.compareAndSet(false, true)) {
+
+                try {
+                    uni = onFailure.apply(resource, failure);
+                    if (uni == null) {
+                        throw new NullPointerException("The finalizer produced a `null` Uni");
+                    }
+                } catch (Throwable e) {
+                    innerError = e;
+                }
+            }
+
+            Subscriptions.cancel(upstream);
+            if (innerError != null) {
+                downstream.onFailure(new CompositeException(failure, innerError));
+            } else if (uni != null) {
+                uni.subscribe().with(
+                        completed -> downstream.onFailure(failure),
+                        failed -> downstream.onFailure(new CompositeException(failure, failed)));
+            }
+        }
+
+        @Override
+        public void onCompletion() {
+            Throwable innerError = null;
+            Uni<Void> uni = null;
+            if (terminated.compareAndSet(false, true)) {
+                try {
+                    uni = onCompletion.apply(resource);
+                    if (uni == null) {
+                        throw new NullPointerException("The finalizer produced a `null` Uni");
+                    }
+                } catch (Throwable e) {
+                    innerError = e;
+                }
+            }
+
+            Subscriptions.cancel(upstream);
+            if (innerError != null) {
+                downstream.onFailure(innerError);
+            } else if (uni != null) {
+                uni.subscribe().with(
+                        completed -> downstream.onCompletion(),
+                        downstream::onFailure);
+            }
+        }
+
+        @Override
+        public void request(long n) {
+            upstream.get().request(n);
+        }
+
+        @Override
+        public void cancel() {
+            Throwable innerError = null;
+            Uni<Void> uni = null;
+            if (terminated.compareAndSet(false, true)) {
+                try {
+                    uni = onCancellation.apply(resource);
+                    if (uni == null) {
+                        throw new NullPointerException("The finalizer produced a `null` Uni");
+                    }
+                } catch (Throwable e) {
+                    innerError = e;
+                }
+            }
+
+            Subscriptions.cancel(upstream);
+            if (innerError != null) {
+                downstream.onFailure(innerError);
+            } else if (uni != null) {
+                uni.subscribe().with(
+                        completed -> {
+                        },
+                        failed -> {
+                        } // ignore the failure, we have cancelled anyway.
+                );
+            }
+        }
+    }
+}
