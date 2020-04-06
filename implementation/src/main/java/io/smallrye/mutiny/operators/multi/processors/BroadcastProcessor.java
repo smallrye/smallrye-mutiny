abf35f8b4b26769c36903cd9a6d210ce0c77971b
Clement Escoffier <clement.escoffier@gmail.com>
2020-04-06 14:31:31 +0200

    Add support for hot streams using a BroadcastProcessor

---
+++ implementation/src/main/java/io/smallrye/mutiny/operators/multi/processors/BroadcastProcessor.java
@@ -0,0 +1,247 @@
+package io.smallrye.mutiny.operators.multi.processors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.reactivestreams.Processor;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.smallrye.mutiny.helpers.ParameterValidation;
+import io.smallrye.mutiny.helpers.Subscriptions;
+import io.smallrye.mutiny.operators.AbstractMulti;
+import io.smallrye.mutiny.subscription.BackPressureFailure;
+import io.smallrye.mutiny.subscription.MultiSubscriber;
+
+/**
+ * Implementation of {@link org.reactivestreams.Processor} that broadcast all subsequently observed items to its current
+ * {@link Subscriber}s.
+ * <p>
+ * This processor does not coordinate back-pressure between different subscribers and between the upstream source and a
+ * subscriber. If an upstream item is received via {@link #onNext(Object)}, if a subscriber is not ready to receive that
+ * item, that subscriber is terminated via a {@link io.smallrye.mutiny.subscription.BackPressureFailure}.
+ * <p>
+ * The {@code BroadcastProcessor}'s {@link Subscriber}-side consumes items in an unbounded manner.
+ * <p>
+ * When this {@code BroadcastProcessor} is terminated via {@link #onError(Throwable)} or {@link #onComplete()}, late
+ * {@link Subscriber}s only receive the respective terminal event.
+ * <p>
+ * Unlike the {@link UnicastProcessor}, a {@code BroadcastProcessor} doesn't retain/cache items, therefore, a new
+ * {@code Subscriber} won't receive any past items.
+ * <p>
+ * Even though {@code BroadcastProcessor} implements the {@link Subscriber} interface, calling {@code onSubscribe} is
+ * not required if the processor is used as a <em>standalone</em> source. However, calling {@code onSubscribe} after
+ * the {@code BroadcastProcessor} has failed or reached completion results in the given {@link Subscription} being
+ * canceled immediately.
+ */
+public class BroadcastProcessor<T> extends AbstractMulti<T> implements Processor<T, T> {
+
+    /**
+     * Value indicating that the upstream has been cancelled.
+     */
+    static final List<?> TERMINATED = new ArrayList<>(0);
+
+    /**
+     * The array of currently subscribed subscribers.
+     */
+    final AtomicReference<List<BroadcastSubscription<T>>> subscribers;
+
+    /**
+     * The failure, write before terminating and read after checking subscribers.
+     */
+    Throwable failure;
+
+    /**
+     * Creates a new {@code BroadcastProcessor}
+     *
+     * @param <T> the type of item
+     * @return the new {@code BroadcastProcessor}
+     */
+    public static <T> BroadcastProcessor<T> create() {
+        return new BroadcastProcessor<>();
+    }
+
+    /**
+     * Constructs a BroadcastProcessor.
+     */
+    private BroadcastProcessor() {
+        subscribers = new AtomicReference<>(new CopyOnWriteArrayList<>());
+    }
+
+    public SerializedProcessor<T, T> serialized() {
+        return new SerializedProcessor<>(this);
+    }
+
+    /**
+     * Tries to add the given subscriber to the subscribers array atomically
+     * or returns {@code false} if this processor has terminated.
+     *
+     * @param sub the subscriber to add
+     * @return {@code true} if successful, {@code false} if this processor has terminated
+     */
+    private boolean addSubscription(BroadcastSubscription<T> sub) {
+        List<BroadcastSubscription<T>> current = subscribers.get();
+        if (current == TERMINATED) {
+            return false;
+        }
+        return current.add(sub);
+    }
+
+    /**
+     * Atomically removes the given subscriber if it is subscribed to this processor.
+     *
+     * @param sub the subscription wrapping a subscriber to remove
+     */
+    void remove(BroadcastSubscription<T> sub) {
+        List<BroadcastSubscription<T>> current = subscribers.get();
+        if (current == TERMINATED || current.isEmpty()) {
+            return;
+        }
+        current.remove(sub);
+    }
+
+    @Override
+    public void subscribe(MultiSubscriber<? super T> downstream) {
+        BroadcastSubscription<T> subscription = new BroadcastSubscription<>(downstream, this);
+        downstream.onSubscribe(subscription);
+        if (addSubscription(subscription)) {
+            // if cancellation happened while a successful add, the remove() didn't work so we need to do it again
+            if (subscription.isCancelled()) {
+                remove(subscription);
+            }
+        } else {
+            Throwable ex = failure;
+            if (ex != null) {
+                downstream.onFailure(ex);
+            } else {
+                downstream.onCompletion();
+            }
+        }
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        if (subscribers.get() == TERMINATED) {
+            subscription.cancel();
+            return;
+        }
+        subscription.request(Long.MAX_VALUE);
+    }
+
+    @Override
+    public void onNext(T item) {
+        ParameterValidation.nonNullNpe(item, "item");
+        for (BroadcastSubscription<T> s : subscribers.get()) {
+            s.onNext(item);
+        }
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    public void onError(Throwable failure) {
+        ParameterValidation.nonNullNpe(failure, "failure");
+        if (subscribers.get() == TERMINATED) {
+            return;
+        }
+        this.failure = failure;
+        List<BroadcastSubscription<?>> andSet = subscribers.getAndSet((List) TERMINATED);
+        for (BroadcastSubscription<?> s : andSet) {
+            s.onError(failure);
+        }
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    public void onComplete() {
+        if (subscribers.get() == TERMINATED) {
+            return;
+        }
+        List<BroadcastSubscription<?>> andSet = subscribers.getAndSet((List) TERMINATED);
+        for (BroadcastSubscription<?> s : andSet) {
+            s.onComplete();
+        }
+    }
+
+    /**
+     * Wraps the actual subscriber, tracks its requests and makes cancellation
+     * to remove itself from the current subscribers array.
+     *
+     * @param <T> the type of item
+     */
+    static final class BroadcastSubscription<T> implements Subscription {
+
+        /**
+         * The actual subscriber.
+         **/
+        private final Subscriber<? super T> downstream;
+
+        /**
+         * The parent processor using this subscriber.
+         */
+        private final BroadcastProcessor<T> parent;
+
+        /**
+         * Pending requests.
+         * {@code Long.MIN_VALUE} indicates cancellation.
+         */
+        private final AtomicLong requests = new AtomicLong();
+
+        /**
+         * Constructs a BroadcastSubscription, wraps the actual subscriber and the state.
+         *
+         * @param actual the actual subscriber
+         * @param parent the parent PublishProcessor
+         */
+        BroadcastSubscription(Subscriber<? super T> actual, BroadcastProcessor<T> parent) {
+            this.downstream = actual;
+            this.parent = parent;
+        }
+
+        public void onNext(T t) {
+            long r = requests.get();
+            if (r == Long.MIN_VALUE) {
+                return;
+            }
+            if (r != 0L) {
+                downstream.onNext(t);
+                Subscriptions.producedAndHandleAlreadyCancelled(requests, 1);
+            } else {
+                cancel();
+                downstream.onError(new BackPressureFailure("Could not emit item downstream due to lack of requests"));
+            }
+        }
+
+        public void onError(Throwable t) {
+            if (requests.get() != Long.MIN_VALUE) {
+                downstream.onError(t);
+            }
+        }
+
+        public void onComplete() {
+            if (requests.get() != Long.MIN_VALUE) {
+                downstream.onComplete();
+            }
+        }
+
+        @Override
+        public void request(long n) {
+            if (n > 0) {
+                Subscriptions.addAndHandledAlreadyCancelled(requests, n);
+            }
+        }
+
+        @Override
+        public void cancel() {
+            if (requests.getAndSet(Long.MIN_VALUE) != Long.MIN_VALUE) {
+                parent.remove(this);
+            }
+        }
+
+        public boolean isCancelled() {
+            return requests.get() == Long.MIN_VALUE;
+        }
+    }
+}
---
+++ implementation/src/test/java/io/smallrye/mutiny/operators/multi/processors/BroadcastProcessorTest.java
@@ -0,0 +1,469 @@
+package io.smallrye.mutiny.operators.multi.processors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.subscription.BackPressureFailure;
+import io.smallrye.mutiny.test.MultiAssertSubscriber;
+
+public class BroadcastProcessorTest {
+
+    private ExecutorService executor;
+
+    @BeforeMethod
+    public void setup() {
+        executor = Executors.newFixedThreadPool(4);
+    }
+
+    @AfterMethod
+    public void cleanup() {
+        executor.shutdownNow();
+    }
+
+    @Test
+    public void testWithTwoSubscribers() {
+        BroadcastProcessor<String> processor = BroadcastProcessor.create();
+
+        MultiAssertSubscriber<String> subscriber1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onNext("one");
+        processor.onNext("two");
+        processor.onNext("three");
+
+        MultiAssertSubscriber<String> subscriber2 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onNext("four");
+        processor.onComplete();
+
+        subscriber1
+                .assertReceived("one", "two", "three", "four")
+                .assertCompletedSuccessfully();
+
+        subscriber2
+                .assertReceived("four")
+                .assertCompletedSuccessfully();
+    }
+
+    @Test
+    public void testWithTwoSubscribersSerialized() {
+        SerializedProcessor<String, String> processor = BroadcastProcessor.<String> create().serialized();
+
+        MultiAssertSubscriber<String> subscriber1 = MultiAssertSubscriber.create(10);
+        MultiAssertSubscriber<String> subscriber2 = MultiAssertSubscriber.create(10);
+
+        processor.subscribe(subscriber1);
+
+        processor.onNext("one");
+        processor.onNext("two");
+        processor.onNext("three");
+
+        processor.subscribe(subscriber2);
+
+        processor.onNext("four");
+        processor.onComplete();
+
+        subscriber1
+                .assertReceived("one", "two", "three", "four")
+                .assertCompletedSuccessfully();
+
+        subscriber2
+                .assertReceived("four")
+                .assertCompletedSuccessfully();
+    }
+
+    @Test
+    public void testSubscriptionAfterCompletion() {
+        BroadcastProcessor<String> processor = BroadcastProcessor.create();
+
+        MultiAssertSubscriber<String> subscriber1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onNext("one");
+        processor.onNext("two");
+        processor.onNext("three");
+
+        MultiAssertSubscriber<String> subscriber2 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onComplete();
+
+        MultiAssertSubscriber<String> subscriber3 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        subscriber1
+                .assertReceived("one", "two", "three")
+                .assertCompletedSuccessfully();
+
+        subscriber2
+                .assertHasNotReceivedAnyItem()
+                .assertCompletedSuccessfully();
+
+        subscriber3
+                .assertHasNotReceivedAnyItem()
+                .assertCompletedSuccessfully();
+    }
+
+    @Test
+    public void testSubscriptionAfterFailure() {
+        BroadcastProcessor<String> processor = BroadcastProcessor.create();
+
+        MultiAssertSubscriber<String> subscriber1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onNext("one");
+        processor.onNext("two");
+        processor.onNext("three");
+
+        MultiAssertSubscriber<String> subscriber2 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onError(new Exception("boom"));
+
+        MultiAssertSubscriber<String> subscriber3 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        subscriber1
+                .assertReceived("one", "two", "three")
+                .assertHasFailedWith(Exception.class, "boom");
+
+        subscriber2
+                .assertHasNotReceivedAnyItem()
+                .assertHasFailedWith(Exception.class, "boom");
+
+        subscriber3
+                .assertHasNotReceivedAnyItem()
+                .assertHasFailedWith(Exception.class, "boom");
+    }
+
+    @Test
+    public void testFailureAfterCompletion() {
+        BroadcastProcessor<Integer> processor = BroadcastProcessor.create();
+        MultiAssertSubscriber<Integer> subscriber = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onNext(1);
+        processor.onNext(2);
+        processor.onComplete();
+        processor.onError(new Exception("boom"));
+
+        subscriber.assertCompletedSuccessfully().assertReceived(1, 2);
+    }
+
+    @Test
+    public void testNoITemAfterCancellation() {
+        BroadcastProcessor<String> processor = BroadcastProcessor.create();
+
+        MultiAssertSubscriber<String> subscriber1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onNext("one");
+
+        subscriber1.assertReceived("one");
+
+        MultiAssertSubscriber<String> subscriber2 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onNext("two");
+
+        subscriber1.assertReceived("one", "two");
+        subscriber2.assertReceived("two");
+
+        subscriber1.cancel();
+
+        processor.onNext("three");
+
+        subscriber1.assertNotTerminated().assertReceived("one", "two");
+        subscriber2.assertReceived("two", "three");
+
+        processor.onComplete();
+
+        subscriber2.assertReceived("two", "three").assertCompletedSuccessfully();
+        subscriber1.assertNotTerminated().assertReceived("one", "two");
+
+        processor.onNext("four");
+
+        subscriber2.assertReceived("two", "three").assertCompletedSuccessfully();
+        subscriber1.assertNotTerminated().assertReceived("one", "two");
+    }
+
+    @Test
+    public void testResubscription() {
+        BroadcastProcessor<String> processor = BroadcastProcessor.create();
+
+        MultiAssertSubscriber<String> subscriber1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onNext("one");
+
+        subscriber1.assertReceived("one");
+        subscriber1.cancel();
+
+        processor.onNext("two");
+
+        processor.subscribe(subscriber1);
+
+        processor.onNext("three");
+        processor.onComplete();
+
+        subscriber1.assertReceived("one", "three")
+                .assertCompletedSuccessfully();
+
+    }
+
+    @Test
+    public void testResubscriptionAfterCompletion() {
+        BroadcastProcessor<String> processor = BroadcastProcessor.create();
+
+        MultiAssertSubscriber<String> subscriber1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onNext("one");
+
+        subscriber1.assertReceived("one");
+        subscriber1.cancel();
+
+        processor.onNext("two");
+        processor.onComplete();
+
+        processor.subscribe(subscriber1);
+
+        subscriber1.assertReceived("one")
+                .assertCompletedSuccessfully();
+
+    }
+
+    @Test
+    public void testResubscriptionAfterFailure() {
+        BroadcastProcessor<String> processor = BroadcastProcessor.create();
+
+        MultiAssertSubscriber<String> subscriber1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        processor.onNext("one");
+
+        subscriber1.assertReceived("one");
+        subscriber1.cancel();
+
+        processor.onNext("two");
+        processor.onError(new IOException("boom"));
+
+        processor.subscribe(subscriber1);
+
+        subscriber1.assertReceived("one")
+                .assertHasFailedWith(IOException.class, "boom");
+
+    }
+
+    @Test
+    public void testUsingTheProcessorWithAnUpstream() {
+        Multi<Integer> upstream = Multi.createFrom().range(0, 10);
+        BroadcastProcessor<Integer> processor = BroadcastProcessor.create();
+
+        MultiAssertSubscriber<Integer> s1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+        MultiAssertSubscriber<Integer> s2 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        upstream.subscribe(processor);
+
+        s1.assertCompletedSuccessfully()
+                .assertReceived(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        s2.assertCompletedSuccessfully()
+                .assertReceived(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    }
+
+    @Test
+    public void testUsingTheProcessorWithAnUpstreamAndLateSubscriber() {
+        Multi<Integer> upstream = Multi.createFrom().range(0, 10);
+        BroadcastProcessor<Integer> processor = BroadcastProcessor.create();
+        upstream.subscribe(processor);
+        MultiAssertSubscriber<Integer> s1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        s1.assertCompletedSuccessfully()
+                .assertHasNotReceivedAnyItem();
+    }
+
+    @Test
+    public void testUsingTheProcessorWithAnUpstreamAlreadyCompleted() {
+        Multi<Integer> upstream = Multi.createFrom().empty();
+        BroadcastProcessor<Integer> processor = BroadcastProcessor.create();
+        upstream.subscribe(processor);
+        MultiAssertSubscriber<Integer> s1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+
+        s1.assertCompletedSuccessfully()
+                .assertHasNotReceivedAnyItem();
+    }
+
+    @Test
+    public void testUsingTheProcessorWithAnUpstreamThatHasAlreadyFailed() {
+        Multi<Integer> upstream = Multi.createFrom().failure(new Exception("boom"));
+        BroadcastProcessor<Integer> processor = BroadcastProcessor.create();
+        upstream.subscribe(processor);
+        MultiAssertSubscriber<Integer> s1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+        MultiAssertSubscriber<Integer> s2 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+        s1.assertHasFailedWith(Exception.class, "boom");
+        s2.assertHasFailedWith(Exception.class, "boom");
+    }
+
+    @Test
+    public void testWhenSubscriberDoesNotHaveRequestedEnough() {
+        BroadcastProcessor<Integer> processor = BroadcastProcessor.create();
+        MultiAssertSubscriber<Integer> s1 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(10));
+        MultiAssertSubscriber<Integer> s2 = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(4));
+
+        for (int i = 0; i < 10; i++) {
+            processor.onNext(i);
+        }
+        processor.onComplete();
+
+        s1.assertCompletedSuccessfully()
+                .assertReceived(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        s2.assertHasFailedWith(BackPressureFailure.class, "request");
+    }
+
+    @Test
+    public void testCrossCancellation() {
+        MultiAssertSubscriber<Integer> subscriber1 = MultiAssertSubscriber.create(10);
+        MultiAssertSubscriber<Integer> subscriber2 = new MultiAssertSubscriber<Integer>(10) {
+            @Override
+            public synchronized void onNext(Integer o) {
+                super.onNext(o);
+                subscriber1.cancel();
+            }
+        };
+        BroadcastProcessor<Integer> processor = BroadcastProcessor.create();
+        processor.subscribe().withSubscriber(subscriber2);
+        processor.subscribe().withSubscriber(subscriber1);
+        processor.onNext(1);
+        subscriber2.assertReceived(1);
+        subscriber1.assertHasNotReceivedAnyItem();
+    }
+
+    @Test
+    public void testCrossCancellationOnFailure() {
+        MultiAssertSubscriber<Integer> subscriber1 = MultiAssertSubscriber.create(10);
+        MultiAssertSubscriber<Integer> subscriber2 = new MultiAssertSubscriber<Integer>(10) {
+            @Override
+            public synchronized void onError(Throwable failure) {
+                super.onError(failure);
+                subscriber1.cancel();
+            }
+        };
+        BroadcastProcessor<Integer> processor = BroadcastProcessor.create();
+        processor.subscribe().withSubscriber(subscriber2);
+        processor.subscribe().withSubscriber(subscriber1);
+        processor.onError(new Exception("boom"));
+        subscriber2.assertHasFailedWith(Exception.class, "boom");
+        subscriber1.assertHasNotReceivedAnyItem().assertNotTerminated();
+    }
+
+    @Test(invocationCount = 100)
+    public void testCompletionRace() {
+        BroadcastProcessor<Integer> processor = BroadcastProcessor.create();
+        MultiAssertSubscriber<Object> subscriber = MultiAssertSubscriber.create(1);
+        processor.subscribe(subscriber);
+
+        final AtomicInteger count = new AtomicInteger(2);
+        Runnable task = () -> {
+            if (count.decrementAndGet() != 0) {
+                while (count.get() != 0) {
+                    // Explicit loop
+                }
+            }
+            processor.onComplete();
+        };
+        executor.submit(task);
+        executor.submit(task);
+
+        subscriber.await(Duration.ofSeconds(5)).assertCompletedSuccessfully();
+    }
+
+    @Test(invocationCount = 100)
+    public void testCompletionVsSubscriptionRace() throws InterruptedException {
+        BroadcastProcessor<Integer> processor = BroadcastProcessor.create();
+        MultiAssertSubscriber<Object> subscriber = MultiAssertSubscriber.create(1);
+
+        CountDownLatch latch = new CountDownLatch(2);
+        final AtomicInteger count = new AtomicInteger(2);
+        Runnable task1 = () -> {
+            if (count.decrementAndGet() != 0) {
+                while (count.get() != 0) {
+                    // Explicit loop
+                }
+            }
+            processor.onComplete();
+            latch.countDown();
+        };
+        Runnable task2 = () -> {
+            if (count.decrementAndGet() != 0) {
+                while (count.get() != 0) {
+                    // Explicit loop
+                }
+            }
+            processor.subscribe(subscriber);
+            latch.countDown();
+        };
+        executor.submit(task1);
+        executor.submit(task2);
+
+        latch.await(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testWithFlatMap() {
+        BroadcastProcessor<Integer> processor = BroadcastProcessor.create();
+        MultiAssertSubscriber<Integer> subscriber = MultiAssertSubscriber.create(10);
+
+        processor
+                .onItem().produceMulti(i -> processor).withRequests(10).merge()
+                .subscribe().withSubscriber(subscriber);
+        processor.onNext(1);
+        processor.onNext(2);
+        processor.onNext(3);
+        processor.onNext(4);
+        processor.onComplete();
+
+        subscriber.assertReceived(2, 3, 3, 4, 4, 4)
+                .assertCompletedSuccessfully();
+
+    }
+
+    @Test
+    public void testMakingTicksAHotStream() throws InterruptedException {
+        Multi<Long> ticks = Multi.createFrom().ticks().every(Duration.ofMillis(1))
+                .transform().byTakingFirstItems(100);
+        BroadcastProcessor<Long> processor = BroadcastProcessor.create();
+        ticks.subscribe().withSubscriber(processor);
+        Thread.sleep(50); // NOSONAR
+
+        MultiAssertSubscriber<Long> subscriber = processor.subscribe()
+                .withSubscriber(MultiAssertSubscriber.create(Long.MAX_VALUE))
+                .await(Duration.ofSeconds(10))
+                .assertCompletedSuccessfully();
+
+        List<Long> items = subscriber.items();
+        assertThat(items).doesNotContain(0L, 1L, 2L, 3L, 4L);
+    }
+}
