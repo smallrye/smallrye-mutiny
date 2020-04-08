7d16775c3ccbe81f3da29620d26eb1b0ec83b9d4
Clement Escoffier <clement.escoffier@gmail.com>
2020-04-08 10:39:18 +0200

    Add Uni.subscribe().with(x -> {})

--- implementation/src/test/java/io/smallrye/mutiny/groups/UniSubscriberTest.java
+++ implementation/src/test/java/io/smallrye/mutiny/groups/UniSubscriberTest.java
@@ -3,15 +3,20 @@ package io.smallrye.mutiny.groups;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
+import java.io.IOException;
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.testng.annotations.Test;
 
 import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.subscription.Cancellable;
 
 public class UniSubscriberTest {
+
     @Test
     public void testCancelSubscription() {
         AtomicLong counter = new AtomicLong();
@@ -35,4 +40,65 @@ public class UniSubscriberTest {
         await().until(() -> counter.intValue() == 0);
         assertThat(failure.get()).isNotNull();
     }
+
+    @Test
+    public void testSingleCallbackVariant() {
+        AtomicInteger result = new AtomicInteger();
+        Uni.createFrom().item(1).subscribe().with(result::set);
+        assertThat(result).hasValue(1);
+
+        AtomicReference<String> value = new AtomicReference<>("sentinel");
+        Uni.createFrom().<String> nullItem().subscribe().with(value::set);
+        assertThat(value).hasValue(null);
+
+        result.set(-1);
+        Uni.createFrom().<Integer> failure(new IOException("boom")).subscribe().with(result::set);
+        assertThat(result).hasValue(-1);
+
+        // Assert cancellation before the emission
+        AtomicBoolean called = new AtomicBoolean();
+        Cancellable cancellable = Uni.createFrom().nothing().subscribe().with(x -> called.set(true));
+        assertThat(called).isFalse();
+        cancellable.cancel();
+        assertThat(called).isFalse();
+
+        // Assert cancellation after the emission
+        cancellable = Uni.createFrom().item(1).subscribe().with(x -> called.set(true));
+        assertThat(called).isTrue();
+        cancellable.cancel();
+    }
+
+    @Test
+    public void testTwoCallbacksVariant() {
+        AtomicInteger result = new AtomicInteger();
+        AtomicReference<Throwable> failure = new AtomicReference<>();
+        Uni.createFrom().item(1).subscribe().with(result::set, failure::set);
+        assertThat(result).hasValue(1);
+        assertThat(failure).hasValue(null);
+
+        AtomicReference<String> value = new AtomicReference<>("sentinel");
+        Uni.createFrom().<String> nullItem().subscribe().with(value::set, failure::set);
+        assertThat(value).hasValue(null);
+        assertThat(failure).hasValue(null);
+
+        result.set(-1);
+        Uni.createFrom().<Integer> failure(new IOException("boom")).subscribe().with(result::set, failure::set);
+        assertThat(result).hasValue(-1);
+        assertThat(failure.get()).isInstanceOf(IOException.class).hasMessage("boom");
+
+        // Assert cancellation before the emission
+        AtomicBoolean called = new AtomicBoolean();
+        Cancellable cancellable = Uni.createFrom().nothing().subscribe()
+                .with(x -> called.set(true), f -> called.set(true));
+        assertThat(called).isFalse();
+        cancellable.cancel();
+        assertThat(called).isFalse();
+
+        // Assert cancellation after the emission
+        cancellable = Uni.createFrom().item(1).subscribe()
+                .with(x -> called.set(true), f -> called.set(true));
+        assertThat(called).isTrue();
+        cancellable.cancel();
+    }
+
 }
