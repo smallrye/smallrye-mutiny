6331f651dfbf94999af2db13e71f9dd1fce7fe2e
Julien Lafourcade <julien.lafourcade@gmail.com>
2020-07-23 23:33:37 +0200

    Created a Caused which can be used to fluently unwrap checked exception previously wrapped by Unchecked utilities

---
+++ implementation/src/test/java/io/smallrye/mutiny/helpers/CausedTest.java
@@ -0,0 +1,94 @@
+package io.smallrye.mutiny.helpers;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.unchecked.Unchecked;
+
+public class CausedTest {
+
+  private static final String GENERIC = "Generic";
+  private static final String SPECIFIC = "Specific";
+
+  @Test
+  public void testWithWrappedCheckedException() {
+    assertEquals(SPECIFIC, Uni.createFrom()
+                              .item(Unchecked.supplier(this::stringSupplierThrowingIOException))
+                              .onFailure(Caused.by(IOException.class))
+                              .recoverWithItem(SPECIFIC)
+                              .onFailure()
+                              .recoverWithItem(GENERIC)
+                              .await()
+                              .indefinitely());
+  }
+
+  @Test
+  public void testWithWrappedCheckedExceptionSubclass() {
+    assertEquals(SPECIFIC, Uni.createFrom()
+                              .item(Unchecked.supplier(this::stringSupplierThrowingFileNotFoundException))
+                              .onFailure(Caused.by(IOException.class))
+                              .recoverWithItem(SPECIFIC)
+                              .onFailure()
+                              .recoverWithItem(GENERIC)
+                              .await()
+                              .indefinitely());
+  }
+
+  @Test
+  public void testWithUncheckedException() {
+    assertEquals(SPECIFIC, Uni.createFrom()
+                              .item(Unchecked.supplier(this::numberSupplierThrowingIllegalArgumentException))
+                              .onFailure(Caused.by(IllegalArgumentException.class))
+                              .recoverWithItem(SPECIFIC)
+                              .onFailure()
+                              .recoverWithItem(GENERIC)
+                              .await()
+                              .indefinitely());
+  }
+
+  @Test
+  public void testWithUncheckedExceptionSubclass() {
+    assertEquals(SPECIFIC, Uni.createFrom()
+                              .item(Unchecked.supplier(this::numberSupplierThrowingNumberFormatException))
+                              .onFailure(Caused.by(IllegalArgumentException.class))
+                              .recoverWithItem(SPECIFIC)
+                              .onFailure()
+                              .recoverWithItem(GENERIC)
+                              .await()
+                              .indefinitely());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testWithNullParameter() {
+    Uni.createFrom()
+       .item(Unchecked.supplier(this::numberSupplierThrowingNumberFormatException))
+       .onFailure(Caused.by(null))
+       .recoverWithItem(SPECIFIC)
+       .onFailure()
+       .recoverWithItem(GENERIC)
+       .await()
+       .indefinitely();
+  }
+
+  private String stringSupplierThrowingIOException() throws IOException {
+    throw new IOException();
+  }
+
+  private String stringSupplierThrowingFileNotFoundException() throws FileNotFoundException {
+    throw new FileNotFoundException();
+  }
+
+  private String numberSupplierThrowingIllegalArgumentException() {
+    throw new IllegalArgumentException();
+  }
+
+  private String numberSupplierThrowingNumberFormatException() {
+    throw new NumberFormatException();
+  }
+
+}
