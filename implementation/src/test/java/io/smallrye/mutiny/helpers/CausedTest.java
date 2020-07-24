22e0e48445a63c8c12399f7d9551faf053dd4487
Julien Ponge <jponge@redhat.com>
2020-07-24 09:58:26 +0200

    Refactoring of #227
    Signed-off-by: Julien Ponge <jponge@redhat.com>

--- implementation/src/test/java/io/smallrye/mutiny/helpers/CausedTest.java
+++ implementation/src/test/java/io/smallrye/mutiny/helpers/CausedTest.java
@@ -12,83 +12,83 @@ import io.smallrye.mutiny.unchecked.Unchecked;
 
 public class CausedTest {
 
-  private static final String GENERIC = "Generic";
-  private static final String SPECIFIC = "Specific";
+    private static final String GENERIC = "Generic";
+    private static final String SPECIFIC = "Specific";
 
-  @Test
-  public void testWithWrappedCheckedException() {
-    assertEquals(SPECIFIC, Uni.createFrom()
-                              .item(Unchecked.supplier(this::stringSupplierThrowingIOException))
-                              .onFailure(Caused.by(IOException.class))
-                              .recoverWithItem(SPECIFIC)
-                              .onFailure()
-                              .recoverWithItem(GENERIC)
-                              .await()
-                              .indefinitely());
-  }
+    @Test
+    public void testWithWrappedCheckedException() {
+        assertEquals(SPECIFIC, Uni.createFrom()
+                .item(Unchecked.supplier(this::stringSupplierThrowingIOException))
+                .onFailure(Caused.by(IOException.class))
+                .recoverWithItem(SPECIFIC)
+                .onFailure()
+                .recoverWithItem(GENERIC)
+                .await()
+                .indefinitely());
+    }
 
-  @Test
-  public void testWithWrappedCheckedExceptionSubclass() {
-    assertEquals(SPECIFIC, Uni.createFrom()
-                              .item(Unchecked.supplier(this::stringSupplierThrowingFileNotFoundException))
-                              .onFailure(Caused.by(IOException.class))
-                              .recoverWithItem(SPECIFIC)
-                              .onFailure()
-                              .recoverWithItem(GENERIC)
-                              .await()
-                              .indefinitely());
-  }
+    @Test
+    public void testWithWrappedCheckedExceptionSubclass() {
+        assertEquals(SPECIFIC, Uni.createFrom()
+                .item(Unchecked.supplier(this::stringSupplierThrowingFileNotFoundException))
+                .onFailure(Caused.by(IOException.class))
+                .recoverWithItem(SPECIFIC)
+                .onFailure()
+                .recoverWithItem(GENERIC)
+                .await()
+                .indefinitely());
+    }
 
-  @Test
-  public void testWithUncheckedException() {
-    assertEquals(SPECIFIC, Uni.createFrom()
-                              .item(Unchecked.supplier(this::numberSupplierThrowingIllegalArgumentException))
-                              .onFailure(Caused.by(IllegalArgumentException.class))
-                              .recoverWithItem(SPECIFIC)
-                              .onFailure()
-                              .recoverWithItem(GENERIC)
-                              .await()
-                              .indefinitely());
-  }
+    @Test
+    public void testWithUncheckedException() {
+        assertEquals(SPECIFIC, Uni.createFrom()
+                .item(Unchecked.supplier(this::numberSupplierThrowingIllegalArgumentException))
+                .onFailure(Caused.by(IllegalArgumentException.class))
+                .recoverWithItem(SPECIFIC)
+                .onFailure()
+                .recoverWithItem(GENERIC)
+                .await()
+                .indefinitely());
+    }
 
-  @Test
-  public void testWithUncheckedExceptionSubclass() {
-    assertEquals(SPECIFIC, Uni.createFrom()
-                              .item(Unchecked.supplier(this::numberSupplierThrowingNumberFormatException))
-                              .onFailure(Caused.by(IllegalArgumentException.class))
-                              .recoverWithItem(SPECIFIC)
-                              .onFailure()
-                              .recoverWithItem(GENERIC)
-                              .await()
-                              .indefinitely());
-  }
+    @Test
+    public void testWithUncheckedExceptionSubclass() {
+        assertEquals(SPECIFIC, Uni.createFrom()
+                .item(Unchecked.supplier(this::numberSupplierThrowingNumberFormatException))
+                .onFailure(Caused.by(IllegalArgumentException.class))
+                .recoverWithItem(SPECIFIC)
+                .onFailure()
+                .recoverWithItem(GENERIC)
+                .await()
+                .indefinitely());
+    }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void testWithNullParameter() {
-    Uni.createFrom()
-       .item(Unchecked.supplier(this::numberSupplierThrowingNumberFormatException))
-       .onFailure(Caused.by(null))
-       .recoverWithItem(SPECIFIC)
-       .onFailure()
-       .recoverWithItem(GENERIC)
-       .await()
-       .indefinitely();
-  }
+    @Test(expected = IllegalArgumentException.class)
+    public void testWithNullParameter() {
+        Uni.createFrom()
+                .item(Unchecked.supplier(this::numberSupplierThrowingNumberFormatException))
+                .onFailure(Caused.by(null))
+                .recoverWithItem(SPECIFIC)
+                .onFailure()
+                .recoverWithItem(GENERIC)
+                .await()
+                .indefinitely();
+    }
 
-  private String stringSupplierThrowingIOException() throws IOException {
-    throw new IOException();
-  }
+    private String stringSupplierThrowingIOException() throws IOException {
+        throw new IOException();
+    }
 
-  private String stringSupplierThrowingFileNotFoundException() throws FileNotFoundException {
-    throw new FileNotFoundException();
-  }
+    private String stringSupplierThrowingFileNotFoundException() throws FileNotFoundException {
+        throw new FileNotFoundException();
+    }
 
-  private String numberSupplierThrowingIllegalArgumentException() {
-    throw new IllegalArgumentException();
-  }
+    private String numberSupplierThrowingIllegalArgumentException() {
+        throw new IllegalArgumentException();
+    }
 
-  private String numberSupplierThrowingNumberFormatException() {
-    throw new NumberFormatException();
-  }
+    private String numberSupplierThrowingNumberFormatException() {
+        throw new NumberFormatException();
+    }
 
 }
