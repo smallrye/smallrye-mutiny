bae97e29e0f13a9470b6d801f071e53d9ae81cce
Clement Escoffier <clement.escoffier@gmail.com>
2020-08-26 10:54:43 +0200

    Use Moditect to generate module-info for the main Mutiny module

---
+++ implementation/src/main/module/module-info.java
@@ -0,0 +1,27 @@
+module io.smallrye.mutiny {
+    requires org.reactivestreams;
+
+    exports io.smallrye.mutiny;
+    exports io.smallrye.mutiny.converters;
+    exports io.smallrye.mutiny.converters.multi;
+    exports io.smallrye.mutiny.converters.uni;
+    exports io.smallrye.mutiny.groups;
+    exports io.smallrye.mutiny.helpers;
+    exports io.smallrye.mutiny.helpers.queues;
+    exports io.smallrye.mutiny.infrastructure;
+
+    exports io.smallrye.mutiny.operators;
+    exports io.smallrye.mutiny.operators.multi;
+    exports io.smallrye.mutiny.operators.multi.builders;
+    exports io.smallrye.mutiny.operators.multi.multicast;
+    exports io.smallrye.mutiny.operators.multi.overflow;
+    exports io.smallrye.mutiny.operators.multi.processors;
+    exports io.smallrye.mutiny.operators.uni.builders;
+
+    exports io.smallrye.mutiny.tuples;
+    exports io.smallrye.mutiny.unchecked;
+
+    uses io.smallrye.mutiny.infrastructure.MultiInterceptor;
+    uses io.smallrye.mutiny.infrastructure.ExecutorConfiguration;
+    uses io.smallrye.mutiny.infrastructure.UniInterceptor;
+}
