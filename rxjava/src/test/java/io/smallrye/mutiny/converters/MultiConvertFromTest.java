60d70704daffc506246306999b7f1f0dfdf3247f
Clement Escoffier <clement.escoffier@gmail.com>
2020-08-30 13:30:47 +0200

    Migrate rxjava converter test to junit 5

--- rxjava/src/test/java/io/smallrye/mutiny/converters/MultiConvertFromTest.java
+++ rxjava/src/test/java/io/smallrye/mutiny/converters/MultiConvertFromTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
--- rxjava/src/test/java/io/smallrye/mutiny/converters/UniConvertToTest.java
+++ rxjava/src/test/java/io/smallrye/mutiny/converters/UniConvertToTest.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import io.reactivex.Completable;
 import io.reactivex.Flowable;
