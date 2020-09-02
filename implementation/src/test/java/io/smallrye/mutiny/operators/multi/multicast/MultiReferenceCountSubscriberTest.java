10ca78d6673042775ee6850ae8e1066c018cbcf3
Clement Escoffier <clement.escoffier@gmail.com>
2020-09-02 14:43:24 +0200

    Verify the behavior of the MultiReferenceCountSubscriber when receiving failures, cancellation and illegal events.

---
+++ implementation/src/test/java/io/smallrye/mutiny/operators/multi/multicast/MultiReferenceCountSubscriberTest.java
@@ -0,0 +1,109 @@
+package io.smallrye.mutiny.operators.multi.multicast;
+
+import static org.mockito.Mockito.mock;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.operators.AbstractMulti;
+import io.smallrye.mutiny.subscription.MultiSubscriber;
+import io.smallrye.mutiny.test.AssertSubscriber;
+
+class MultiReferenceCountSubscriberTest {
+
+    @Test
+    public void testCancellationWithASingleSubscriber() {
+        Multi<Integer> multi = Multi.createFrom().items(1, 2, 3, 4, 5)
+                .broadcast().withCancellationAfterLastSubscriberDeparture().toAllSubscribers();
+
+        AssertSubscriber<Integer> subscriber = multi.subscribe()
+                .withSubscriber(AssertSubscriber.create(1));
+
+        subscriber.request(2).cancel();
+        subscriber.assertReceived(1, 2, 3);
+    }
+
+    @Test
+    public void testCancellationWithATwoSubscribers() {
+        Multi<Integer> multi = Multi.createFrom().items(1, 2, 3, 4, 5)
+                .broadcast().withCancellationAfterLastSubscriberDeparture().toAllSubscribers();
+
+        AssertSubscriber<Integer> subscriber1 = multi.subscribe()
+                .withSubscriber(AssertSubscriber.create(0));
+        AssertSubscriber<Integer> subscriber2 = multi.subscribe()
+                .withSubscriber(AssertSubscriber.create(0));
+
+        subscriber2.request(3);
+        subscriber1.request(3).cancel();
+        subscriber1.assertReceived(1, 2, 3);
+        subscriber2.request(20);
+        subscriber2.assertReceived(1, 2, 3, 4, 5);
+    }
+
+    @Test
+    public void testFailureWithASingleSubscriber() {
+        Multi<Integer> multi = Multi.createBy().concatenating().streams(
+                Multi.createFrom().items(1, 2, 3, 4, 5),
+                Multi.createFrom().failure(new IOException("boom")))
+                .broadcast()
+                .withCancellationAfterLastSubscriberDeparture().toAllSubscribers();
+
+        AssertSubscriber<Integer> subscriber = multi.subscribe()
+                .withSubscriber(AssertSubscriber.create(Long.MAX_VALUE));
+
+        subscriber
+                .assertHasFailedWith(IOException.class, "boom");
+
+    }
+
+    @Test
+    public void testFailureWithATwoSubscribers() {
+        Multi<Integer> multi = Multi.createBy().concatenating().streams(
+                Multi.createFrom().items(1, 2, 3, 4, 5),
+                Multi.createFrom().failure(new IOException("boom")))
+                .broadcast()
+                .withCancellationAfterLastSubscriberDeparture().toAllSubscribers();
+
+        AssertSubscriber<Integer> subscriber1 = multi.subscribe()
+                .withSubscriber(AssertSubscriber.create(Long.MAX_VALUE));
+        AssertSubscriber<Integer> subscriber2 = multi.subscribe()
+                .withSubscriber(AssertSubscriber.create(Long.MAX_VALUE));
+
+        subscriber1
+                .assertHasFailedWith(IOException.class, "boom");
+
+        subscriber2
+                .assertHasFailedWith(IOException.class, "boom");
+    }
+
+    @Test
+    public void testFailureWithRogueUpstream() {
+        Multi<Integer> multi = new AbstractMulti<Integer>() {
+            @Override
+            public void subscribe(MultiSubscriber<? super Integer> subscriber) {
+                subscriber.onSubscribe(mock(Subscription.class));
+                subscriber.onItem(1);
+                subscriber.onItem(2);
+                subscriber.onItem(3);
+                subscriber.onFailure(new IOException("boom"));
+                subscriber.onItem(4);
+                subscriber.onItem(5);
+                subscriber.onComplete();
+                subscriber.onItem(6);
+            }
+        }
+                .broadcast()
+                .withCancellationAfterLastSubscriberDeparture().toAllSubscribers();
+
+        AssertSubscriber<Integer> subscriber = multi.subscribe()
+                .withSubscriber(AssertSubscriber.create(Long.MAX_VALUE));
+
+        subscriber
+                .assertHasFailedWith(IOException.class, "boom");
+
+    }
+
+}
