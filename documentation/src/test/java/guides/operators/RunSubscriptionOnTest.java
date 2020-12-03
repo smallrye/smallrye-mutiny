483e770ef65d6f1c4f6047c8e294481f5e255887
Clement Escoffier <clement@apache.org>
2020-12-03 10:11:50 +0100

    Migrate unicast, host streams, polling and emitOn/subscribeOn documentation to the new web site

---
+++ documentation/src/test/java/guides/operators/RunSubscriptionOnTest.java
@@ -0,0 +1,67 @@
+package guides.operators;
+
+import guides.extension.SystemOut;
+import guides.extension.SystemOutCaptureExtension;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
+
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.CoreMatchers.is;
+
+@SuppressWarnings("Convert2MethodRef")
+@ExtendWith(SystemOutCaptureExtension.class)
+public class RunSubscriptionOnTest {
+
+    @Test
+    public void testRunSubscriptionOn(SystemOut out) {
+        Executor executor = Infrastructure.getDefaultExecutor();
+        AtomicBoolean completed = new AtomicBoolean();
+        //tag::runSubscriptionOn[]
+        Multi.createFrom().items(() -> {
+            // called on a thread from the executor
+            return retrieveItemsFromSource();
+        })
+                .onItem().transform(this::applySomeOperation)
+                .runSubscriptionOn(executor)
+                .subscribe().with(
+                item -> System.out.println("Item: " + item),
+                Throwable::printStackTrace,
+                () -> completed.set(true)
+        );
+        //end::runSubscriptionOn[]
+
+        await().untilAtomic(completed, is(true));
+    }
+
+    @Test
+    public void testEmitOn(SystemOut out) {
+        Executor executor = Infrastructure.getDefaultExecutor();
+        AtomicBoolean completed = new AtomicBoolean();
+        //tag::emitOn[]
+        Multi.createFrom().items(this::retrieveItemsFromSource)
+                .emitOn(executor)
+                .onItem().transform(this::applySomeOperation)
+                .subscribe().with(
+                item -> System.out.println("Item: " + item),
+                Throwable::printStackTrace,
+                () -> completed.set(true)
+        );
+        //end::emitOn[]
+
+        await().untilAtomic(completed, is(true));
+    }
+
+    public String applySomeOperation(String s) {
+        return s.toUpperCase();
+    }
+
+    public Stream<String> retrieveItemsFromSource() {
+        return Stream.of("a", "b", "c");
+    }
+}
