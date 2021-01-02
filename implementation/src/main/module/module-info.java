bc653433f222e1eb89ed4dad89b2e244553c0fd7
Clement Escoffier <clement.escoffier@gmail.com>
2021-01-02 09:57:27 +0100

    Add helpers.{spies,test} and subscription packages in the module-info.java
    Fix https://github.com/smallrye/smallrye-mutiny/issues/411

--- implementation/src/main/module/module-info.java
+++ implementation/src/main/module/module-info.java
@@ -2,12 +2,18 @@ module io.smallrye.mutiny {
     requires org.reactivestreams;
 
     exports io.smallrye.mutiny;
+
     exports io.smallrye.mutiny.converters;
     exports io.smallrye.mutiny.converters.multi;
     exports io.smallrye.mutiny.converters.uni;
+
     exports io.smallrye.mutiny.groups;
+
     exports io.smallrye.mutiny.helpers;
     exports io.smallrye.mutiny.helpers.queues;
+    exports io.smallrye.mutiny.helpers.spies;
+    exports io.smallrye.mutiny.helpers.test;
+
     exports io.smallrye.mutiny.infrastructure;
 
     exports io.smallrye.mutiny.operators;
@@ -18,6 +24,7 @@ module io.smallrye.mutiny {
     exports io.smallrye.mutiny.operators.multi.processors;
     exports io.smallrye.mutiny.operators.uni.builders;
 
+    exports io.smallrye.mutiny.subscription;
     exports io.smallrye.mutiny.tuples;
     exports io.smallrye.mutiny.unchecked;
 
