228b87350626f218171e36381ed63d63f5b0677f
Julien Ponge <jponge@redhat.com>
2021-03-25 09:59:39 +0100

    Cleanly handle skipped tests with an Assumption

--- reactive-streams-junit5-tck/src/test/java/org/reactivestreams/tck/junit5/TestngSkippingExtension.java
+++ reactive-streams-junit5-tck/src/test/java/org/reactivestreams/tck/junit5/TestngSkippingExtension.java
@@ -1,15 +1,20 @@
 package org.reactivestreams.tck.junit5;
 
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 import org.testng.SkipException;
 
 public class TestngSkippingExtension implements TestExecutionExceptionHandler {
 
+    private static final Logger logger = Logger.getLogger(TestngSkippingExtension.class.getName());
+
     @Override
     public void handleTestExecutionException(ExtensionContext extensionContext, Throwable throwable) throws Throwable {
         if (throwable instanceof SkipException) {
-            System.out.println("[skip] " + extensionContext.getDisplayName() + " -> " + throwable.getMessage());
+            Assumptions.assumeTrue(false, "[skip] " + extensionContext.getDisplayName() + " -> " + throwable.getMessage());
         } else {
             throw throwable;
         }
