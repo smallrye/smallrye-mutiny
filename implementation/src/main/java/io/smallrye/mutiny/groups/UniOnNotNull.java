378403f12476c9e99fdc19ae2bbb947b8fab1e1d
Alexandre Guidin <alexandre.guidin@gmail.com>
2021-07-16 20:52:05 -0300

    fixes

--- implementation/src/main/java/io/smallrye/mutiny/groups/UniOnNotNull.java
+++ implementation/src/main/java/io/smallrye/mutiny/groups/UniOnNotNull.java
@@ -9,6 +9,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import io.smallrye.mutiny.helpers.ParameterValidation;
 import org.reactivestreams.Publisher;
 
 import io.smallrye.mutiny.Multi;
@@ -102,7 +103,7 @@ public class UniOnNotNull<T> {
      * For asynchronous composition, see {@link #transformToUni(Function)}.
      *
      * @param mapper the mapper function, must not be {@code null}
-     * @param <R> the type of Uni item
+     * @param <R>    the type of Uni item
      * @return the new {@link Uni}
      */
     public <R> Uni<R> transform(Function<? super T, ? extends R> mapper) {
@@ -129,10 +130,10 @@ public class UniOnNotNull<T> {
      * This operation is generally named {@code flatMap}.
      *
      * @param mapper the function called with the item of this {@link Uni} and producing the {@link Uni},
-     *        must not be {@code null}, must not return {@code null}.
-     * @param <R> the type of item
+     *               must not be {@code null}, must not return {@code null}.
+     * @param <R>    the type of item
      * @return a new {@link Uni} that would fire events from the uni produced by the mapper function, possibly
-     *         in an asynchronous manner.
+     * in an asynchronous manner.
      */
     public <R> Uni<R> transformToUni(Function<? super T, Uni<? extends R>> mapper) {
         Function<? super T, Uni<? extends R>> actual = Infrastructure.decorate(nonNull(mapper, "mapper"));
@@ -158,7 +159,7 @@ public class UniOnNotNull<T> {
      * This operation is generally named {@code flatMapPublisher}.
      *
      * @param mapper the mapper, must not be {@code null}, may expect to receive {@code null} as item.
-     * @param <R> the type of item produced by the resulting {@link Multi}
+     * @param <R>    the type of item produced by the resulting {@link Multi}
      * @return the multi
      */
     public <R> Multi<R> transformToMulti(Function<? super T, ? extends Publisher<? extends R>> mapper) {
@@ -184,10 +185,10 @@ public class UniOnNotNull<T> {
      * downstream.
      *
      * @param consumer the function called with the item of the this {@link Uni} and an {@link UniEmitter}.
-     *        It must not be {@code null}.
-     * @param <R> the type of item emitted by the emitter
+     *                 It must not be {@code null}.
+     * @param <R>      the type of item emitted by the emitter
      * @return a new {@link Uni} that would fire events from the emitter consumed by the mapper function, possibly
-     *         in an asynchronous manner.
+     * in an asynchronous manner.
      */
     public <R> Uni<R> transformToUni(BiConsumer<? super T, UniEmitter<? super R>> consumer) {
         BiConsumer<? super T, UniEmitter<? super R>> actual = Infrastructure.decorate(nonNull(consumer, "consumer"));
@@ -201,7 +202,7 @@ public class UniOnNotNull<T> {
     }
 
     /**
-     * If the current {@link Uni} contains a item, the produced {@link Uni} emits a failure produced
+     * If the current {@link Uni} contains an item, the produced {@link Uni} emits a failure produced
      * using the given {@link Supplier}.
      *
      * @param supplier the supplier to produce the failure, must not be {@code null}, must not produce {@code null}
@@ -209,14 +210,14 @@ public class UniOnNotNull<T> {
      */
     public Uni<T> failWith(Supplier<? extends Throwable> supplier) {
         Supplier<? extends Throwable> actual = Infrastructure.decorate(nonNull(supplier, "supplier"));
-        return Infrastructure.onUniCreation(transformToUni(ignored -> {
-            Throwable failure = Objects.requireNonNull(actual.get(), SUPPLIER_PRODUCED_NULL);
+        return transformToUni(ignored -> {
+            Throwable failure = ParameterValidation.nonNull(actual.get(), "suplier");
             return Uni.createFrom().failure(failure);
-        }));
+        });
     }
 
     /**
-     * If the current {@link Uni} contains a item, the produced {@link Uni} emits the passed failure.
+     * If the current {@link Uni} contains an item, the produced {@link Uni} emits the passed failure.
      *
      * @param failure the exception to fire if the current {@link Uni} contains a item.
      * @return the new {@link Uni}
