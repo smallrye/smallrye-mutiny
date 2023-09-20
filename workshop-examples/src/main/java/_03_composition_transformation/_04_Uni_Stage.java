2828fa5d01e18fef6033e1e2170d4fc4be9a103c
jponge <smallrye@googlegroups.com>
2023-09-20 19:08:47 +0000

    chore(release): release Mutiny 2.5.0-M1

--- workshop-examples/src/main/java/_03_composition_transformation/_04_Uni_Stage.java
+++ workshop-examples/src/main/java/_03_composition_transformation/_04_Uni_Stage.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.4.0
+//DEPS io.smallrye.reactive:mutiny:2.5.0-M1
 package _03_composition_transformation;
 
 import java.io.IOException;
--- workshop-examples/src/main/java/_08_misc/_02_Logging.java
+++ workshop-examples/src/main/java/_08_misc/_02_Logging.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.4.0
+//DEPS io.smallrye.reactive:mutiny:2.5.0-M1
 package _08_misc;
 
 import io.smallrye.mutiny.Multi;
