d49d0d10033f14dafd13eb979d48927d2912ae42
smallrye-ci <smallrye@googlegroups.com>
2023-09-21 11:05:57 +0000

    chore(release): release Mutiny 2.5.0-M2

--- workshop-examples/src/main/java/_01_basics/_01_Uni.java
+++ workshop-examples/src/main/java/_01_basics/_01_Uni.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.5.0-M1
+//DEPS io.smallrye.reactive:mutiny:2.5.0-M2
 package _01_basics;
 
 import io.smallrye.mutiny.Uni;
--- workshop-examples/src/main/java/_03_composition_transformation/_14_Multi_Aggregates.java
+++ workshop-examples/src/main/java/_03_composition_transformation/_14_Multi_Aggregates.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.5.0-M1
+//DEPS io.smallrye.reactive:mutiny:2.5.0-M2
 package _03_composition_transformation;
 
 import java.util.Arrays;
