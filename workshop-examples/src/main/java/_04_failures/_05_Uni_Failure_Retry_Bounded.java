fac67b1f193c1e1c433b0a79b26ed0ee00ce2be8
smallrye-ci <smallrye@googlegroups.com>
2023-09-21 12:41:02 +0000

    chore(release): release Mutiny 2.5.0-M3

--- workshop-examples/src/main/java/_04_failures/_05_Uni_Failure_Retry_Bounded.java
+++ workshop-examples/src/main/java/_04_failures/_05_Uni_Failure_Retry_Bounded.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.5.0-M2
+//DEPS io.smallrye.reactive:mutiny:2.5.0-M3
 package _04_failures;
 
 import java.util.concurrent.ThreadLocalRandom;
--- workshop-examples/src/main/java/_06_threading/_02_Threading_Emit.java
+++ workshop-examples/src/main/java/_06_threading/_02_Threading_Emit.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.5.0-M2
+//DEPS io.smallrye.reactive:mutiny:2.5.0-M3
 package _06_threading;
 
 import static java.util.concurrent.CompletableFuture.delayedExecutor;
