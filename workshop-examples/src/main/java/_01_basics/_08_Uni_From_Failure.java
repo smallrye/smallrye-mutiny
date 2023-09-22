8f4474cce8245ec030d37e9b6a08fce9fc424078
smallrye-ci <smallrye@googlegroups.com>
2023-09-22 09:23:18 +0000

    chore(release): release Mutiny 2.5.0-M5

--- workshop-examples/src/main/java/_01_basics/_08_Uni_From_Failure.java
+++ workshop-examples/src/main/java/_01_basics/_08_Uni_From_Failure.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.5.0-M4
+//DEPS io.smallrye.reactive:mutiny:2.5.0-M5
 package _01_basics;
 
 import java.io.IOException;
--- workshop-examples/src/main/java/_08_misc/_03_Context.java
+++ workshop-examples/src/main/java/_08_misc/_03_Context.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.5.0-M4
+//DEPS io.smallrye.reactive:mutiny:2.5.0-M5
 package _08_misc;
 
 import java.util.List;
