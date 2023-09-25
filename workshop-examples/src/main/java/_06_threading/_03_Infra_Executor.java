4a69199d2a539a4ade73c9cae21fb2a1f7ee13b6
smallrye-ci <smallrye@googlegroups.com>
2023-09-25 09:12:17 +0000

    chore(release): release Mutiny 2.5.0

--- workshop-examples/src/main/java/_06_threading/_03_Infra_Executor.java
+++ workshop-examples/src/main/java/_06_threading/_03_Infra_Executor.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.5.0-M5
+//DEPS io.smallrye.reactive:mutiny:2.5.0
 package _06_threading;
 
 import static java.util.concurrent.CompletableFuture.supplyAsync;
--- workshop-examples/src/main/java/_06_threading/_05_Threading_Blocking_Check.java
+++ workshop-examples/src/main/java/_06_threading/_05_Threading_Blocking_Check.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.5.0-M5
+//DEPS io.smallrye.reactive:mutiny:2.5.0
 package _06_threading;
 
 import java.util.stream.Collectors;
