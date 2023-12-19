224b63efd892466ca017906b1ab851970b8f7a28
Cesar Alvernaz <cesar.alvernaz@workday.com>
2023-12-19 10:31:46 +0000

    add function interface to assertItem

--- documentation/src/test/java/guides/TestSubscribersTest.java
+++ documentation/src/test/java/guides/TestSubscribersTest.java
@@ -54,19 +54,16 @@ public class TestSubscribersTest {
 
     @Test
     void uniFunction() {
-        // <uni-function>
         Uni<Integer> uni = Uni.createFrom().item(63);
 
         UniAssertSubscriber<Integer> subscriber = uni
             .subscribe().withSubscriber(UniAssertSubscriber.create());
 
         subscriber.assertSubscribed().assertItem(String::valueOf, "63");
-        // </uni-function>
     }
 
     @Test
     void uniFunctionWithObjects() {
-        // <uni-function-with-objects>
         class Person {
             String name;
             int age;
@@ -82,6 +79,5 @@ public class TestSubscribersTest {
 
         subscriber.assertSubscribed().assertItem(p -> p.name, person.name);
         subscriber.assertSubscribed().assertItem(p -> p.age, person.age);
-        // </uni-function-with-objects>
     }
 }
