dd320dca6416bb1557300c60997b9178a9309829
smallrye-ci <smallrye@googlegroups.com>
2024-03-06 11:33:48 +0000

    chore(release): release Mutiny 2.5.8

--- workshop-examples/src/main/java/_03_composition_transformation/_04_Uni_Stage.java
+++ workshop-examples/src/main/java/_03_composition_transformation/_04_Uni_Stage.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.5.7
+//DEPS io.smallrye.reactive:mutiny:2.5.8
 package _03_composition_transformation;
 
 import java.io.IOException;
