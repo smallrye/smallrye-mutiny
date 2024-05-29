94f5a4dc52f64a9ea295670a00d04d596c5c374f
Julien Ponge <jponge@redhat.com>
2024-05-29 09:53:23 +0200

    ci: use the new Smallrye secrets scheme

--- .build/decrypt-secrets.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Decrypting smallrye signature"
-gpg --quiet --batch --yes --decrypt --passphrase="${SECRET_FILES_PASSPHRASE}" \
-    --output smallrye-sign.asc .build/smallrye-sign.asc.gpg
-
-echo "Decrypting Maven settings"
-gpg --quiet --batch --yes --decrypt --passphrase="${SECRET_FILES_PASSPHRASE}" \
-    --output maven-settings.xml .build/maven-settings.xml.gpg
