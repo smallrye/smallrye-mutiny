1df7306cd4782f33bd9e6522d0b64c1f50f60971
Julien Ponge <jponge@redhat.com>
2024-05-29 18:06:16 +0200

    feat: produce a no-dependencies shaded Mutiny jar
    Ref: #1577

---
+++ no-deps/src/main/java/io/smallrye/mutiny/nodeps/Empty.java
@@ -0,0 +1,8 @@
+package io.smallrye.mutiny.nodeps;
+
+/**
+ * This is used as a placeholder to force the maven-javadoc-plugin to create a javadoc jar.
+ * Yes, you read it right :-)
+ */
+public interface Empty {
+}
