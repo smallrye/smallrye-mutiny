b6b0934ee5594630347079426d2a1721d08ab3d9
smallrye-ci <smallrye@googlegroups.com>
2024-07-12 08:25:20 +0000

    chore(release): release Mutiny 2.6.2

--- workshop-examples/src/main/java/_01_basics/_17_Multi_By_Repeating.java
+++ workshop-examples/src/main/java/_01_basics/_17_Multi_By_Repeating.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.6.1
+//DEPS io.smallrye.reactive:mutiny:2.6.2
 package _01_basics;
 
 import java.util.concurrent.CompletableFuture;
--- workshop-examples/src/main/java/_03_composition_transformation/_13_Multi_Broadcast.java
+++ workshop-examples/src/main/java/_03_composition_transformation/_13_Multi_Broadcast.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.6.1
+//DEPS io.smallrye.reactive:mutiny:2.6.2
 package _03_composition_transformation;
 
 import java.util.concurrent.Executors;
