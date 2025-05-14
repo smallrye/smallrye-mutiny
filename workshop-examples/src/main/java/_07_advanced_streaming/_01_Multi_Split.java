9249fd5b373f14bdbd32eb9103aaf9cedf17e1b0
Julien Ponge <jponge@redhat.com>
2025-05-14 11:27:47 +0200

    chore(release): update version metadata for Mutiny 2.9.0-RC1

--- workshop-examples/src/main/java/_07_advanced_streaming/_01_Multi_Split.java
+++ workshop-examples/src/main/java/_07_advanced_streaming/_01_Multi_Split.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.8.0
+//DEPS io.smallrye.reactive:mutiny:2.9.0-RC1
 package _07_advanced_streaming;
 
 import static _07_advanced_streaming._01_Multi_Split.Country.*;
