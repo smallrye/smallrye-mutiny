96509505a0ddd3274dd851be57f246da2090060d
Julien Ponge <jponge@ibm.com>
2025-10-27 12:04:48 +0100

    chore(release): update version metadata for Mutiny 2.9.5

--- workshop-examples/src/main/java/_01_basics/_14_Multi_Subscriber.java
+++ workshop-examples/src/main/java/_01_basics/_14_Multi_Subscriber.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.9.4
+//DEPS io.smallrye.reactive:mutiny:2.9.5
 package _01_basics;
 
 import java.util.concurrent.Flow.Subscriber;
