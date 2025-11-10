b191973638bf3bfa9bcb196da5fea7fde8467bbe
Ozan Gunalp <ozangunalp@gmail.com>
2025-11-10 21:25:07 +0100

    refactor: simplify flatMap upstream request fix

--- implementation/src/main/java/io/smallrye/mutiny/operators/multi/MultiFlatMapOp.java
+++ implementation/src/main/java/io/smallrye/mutiny/operators/multi/MultiFlatMapOp.java
@@ -223,10 +223,7 @@ public final class MultiFlatMapOp<I, O> extends AbstractMultiOperator<I, O> {
                     }
 
                     inner.request(1);
-                    int queuedInners = nonEmptyInnerQueues();
-                    if (queuedInners == 0) {
-                        upstream.request(1);
-                    }
+                    upstream.request(1);
                 } else {
                     if (q == null) {
                         q = getOrCreateInnerQueue(inner);
@@ -364,7 +361,7 @@ public final class MultiFlatMapOp<I, O> extends AbstractMultiOperator<I, O> {
                                 d = inner.done;
                                 boolean empty = q.isEmpty();
                                 if (!d && empty && replenishMain == 0) {
-                                    //                                    replenishMain++;
+                                    replenishMain++;
                                 }
                             }
                         }
@@ -411,11 +408,6 @@ public final class MultiFlatMapOp<I, O> extends AbstractMultiOperator<I, O> {
                             replenishMain++;
                         }
                     }
-                }
-
-                int queuedInners = nonEmptyInnerQueues();
-                if (queuedInners == 0) {
-                    replenishMain++;
                 }
 
                 if (replenishMain != 0L && !done && !cancelled) {
@@ -523,25 +515,6 @@ public final class MultiFlatMapOp<I, O> extends AbstractMultiOperator<I, O> {
                 inner.queue = q;
             }
             return q;
-        }
-
-        int nonEmptyInnerQueues() {
-            FlatMapInner<O>[] inners = get();
-            if (inners == null || isEmpty()) {
-                return -1;
-            }
-            int queuedInners = 0;
-            for (int i = 0; i < inners.length; i++) {
-                FlatMapInner<O> inner = inners[i];
-                if (inner == null || inner.done) {
-                    continue;
-                }
-                Queue<O> q = inner.queue;
-                if (q != null && !q.isEmpty()) {
-                    queuedInners++;
-                }
-            }
-            return queuedInners;
         }
 
         @Override
