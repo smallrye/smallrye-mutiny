c119c9152eb572519ff70aa0879940fbda2595cb
Julien Ponge <jponge@ibm.com>
2025-12-03 09:05:34 +0100

    chore(release): update version metadata for Mutiny 3.1.0

--- workshop-examples/src/main/java/_03_composition_transformation/_09_Multi_Shortcuts.java
+++ workshop-examples/src/main/java/_03_composition_transformation/_09_Multi_Shortcuts.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:3.0.3
+//DEPS io.smallrye.reactive:mutiny:3.1.0
 package _03_composition_transformation;
 
 import static java.util.concurrent.CompletableFuture.delayedExecutor;
