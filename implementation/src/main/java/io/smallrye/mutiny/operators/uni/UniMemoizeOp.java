e6f648a541ccb249274e3538db1bc78c7fe17806
Julien Ponge <jponge@ibm.com>
2026-01-13 10:57:01 +0100

    Revert "fix: context capture should be performed just once in UniMemoizeOp"
    This reverts commit f9efd778ce4ce75cc1a0195b409ed33c52f2af0c.

--- implementation/src/main/java/io/smallrye/mutiny/operators/uni/UniMemoizeOp.java
+++ implementation/src/main/java/io/smallrye/mutiny/operators/uni/UniMemoizeOp.java
@@ -19,7 +19,7 @@ public class UniMemoizeOp<I> extends UniOperator<I, I> implements UniSubscriber<
 
     private UniSubscription currentUpstreamSubscription;
 
-    private Context context = Context.empty();
+    private Context currentContext = Context.empty();
 
     private enum State {
         INIT,
@@ -49,7 +49,6 @@ public class UniMemoizeOp<I> extends UniOperator<I, I> implements UniSubscriber<
     @Override
     public void subscribe(UniSubscriber<? super I> subscriber) {
         nonNull(subscriber, "subscriber");
-        context = subscriber.context();
 
         boolean shouldSubscribeUpstream = false;
         Object cached = null;
@@ -62,6 +61,7 @@ public class UniMemoizeOp<I> extends UniOperator<I, I> implements UniSubscriber<
                 case INIT:
                     state = State.WAITING_FOR_UPSTREAM;
                     awaiters.add(subscriber);
+                    currentContext = subscriber.context();
                     shouldSubscribeUpstream = true;
                     break;
                 case WAITING_FOR_UPSTREAM:
@@ -158,7 +158,7 @@ public class UniMemoizeOp<I> extends UniOperator<I, I> implements UniSubscriber<
 
     @Override
     public Context context() {
-        return context;
+        return currentContext;
     }
 
     private class MemoizedSubscription implements UniSubscription {
